d984389bf2fd46b3d9284e15a5d952728dc47b54
Josua Kiviranta <josua.kiviranta@aalto.fi>
2019-08-22 14:28:48 +0300

    3.1 puhelinluettelon backend step1

---
+++ osa3/puhelinluettelobackend/index.js
@@ -0,0 +1,34 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4
+  }
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+  })
+  
+  app.get('/persons', (req, res) => {
+    res.json(persons)
+  })
+
+const port = 3001
+app.listen(port, () => {
+console.log(`Server running on port ${port}`)
+})
