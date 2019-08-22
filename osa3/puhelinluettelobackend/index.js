f9dac2fb73a319fdb64a658b47643e0cbb643324
Josua Kiviranta <josua.kiviranta@aalto.fi>
2019-08-22 22:35:51 +0300

    3.8*: puhelinluettelon backend step8

--- osa3/puhelinluettelobackend/index.js
+++ osa3/puhelinluettelobackend/index.js
@@ -5,19 +5,12 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan')
 
 app.use(bodyParser.json())
-app.use(morgan('tiny'))
 
-/*
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-} 
+morgan.token('body', function (req, res) {
+  return JSON.stringify(req.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-app.use(requestLogger)
-*/
 
 let persons = [
   {
