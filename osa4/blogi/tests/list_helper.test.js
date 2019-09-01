44efca1c0ef96ee4430cdd13b535b1194262d9c2
Josua Kiviranta <josua.kiviranta@aalto.fi>
2019-09-01 12:06:13 +0300

    4.5*: apufunktioita ja yksikkötestejä, step3

--- osa4/blogi/tests/list_helper.test.js
+++ osa4/blogi/tests/list_helper.test.js
@@ -1,64 +1,100 @@
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-  const blogs = []
+const listWithOneBlog = [
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+    }
+]
 
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
+const listWithThreeBlogs = [
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+    },
+    {
+        _id: '5a422aa71b54a676234d17f9',
+        title: "Testi blogi",
+        author: "Josua",
+        url: "josu.blogi.fi",
+        likes: 42,
+        __v: 0
+    },
+    {
+        _id: '5a422aa71b54a676234d12f8',
+        title: "Testi blogi",
+        author: "Josuaki",
+        url: "josuaki.blogi.fi",
+        likes: 43,
+        __v: 0
+    }
+]
+
+
+test('dummy returns one', () => {
+    const blogs = []
+
+    const result = listHelper.dummy(blogs)
+    expect(result).toBe(1)
 })
 
 describe('totalLikes', () => {
-    const listWithOneBlog = [
-        {
-          _id: '5a422aa71b54a676234d17f8',
-          title: 'Go To Statement Considered Harmful',
-          author: 'Edsger W. Dijkstra',
-          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-          likes: 5,
-          __v: 0
-        }
-      ]
 
-    const listWithThreeBlogs = [
-        {
-            _id: '5a422aa71b54a676234d17f8',
-            title: 'Go To Statement Considered Harmful',
-            author: 'Edsger W. Dijkstra',
-            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-            likes: 5,
-            __v: 0
-          },
-          {
-              _id: '5a422aa71b54a676234d17f9',
-              title: "Testi blogi",
-              author: "Josua",
-              url: "josu.blogi.fi",
-              likes: 42,
-              __v: 0
-            },
-            {
-              _id: '5a422aa71b54a676234d12f8',
-              title: "Testi blogi",
-              author: "Josuaki",
-              url: "josuaki.blogi.fi",
-              likes: 42,
-              __v: 0
-            }
-    ]
-    
-      test('of empty list is zero (1ms)', () => {
-          expect(listHelper.totalLikes([])).toBe(0)
-      })
+    test('of empty list is zero', () => {
+        expect(listHelper.totalLikes([])).toBe(0)
+    })
 
-      test('when list has only one blog equals the likes of that', () => {
+    test('when list has only one blog equals the likes of that', () => {
         const result = listHelper.totalLikes(listWithOneBlog)
         expect(result).toBe(5)
-      })
+    })
 
-      test('of a bigger list is calculated right', () => {
-          const result = listHelper.totalLikes(listWithThreeBlogs)
-          expect(result).toBe(89)
-      })
+    test('of a bigger list is calculated right', () => {
+        const result = listHelper.totalLikes(listWithThreeBlogs)
+        expect(result).toBe(90)
+    })
+})
 
+describe('favouriteBlog', () => {
+
+    test('of empty list is {}', () => {
+        expect(listHelper.favoriteBlog([])).toEqual({})
+    })
+
+    test('when list has only one blog equal object of that', () => {
+        const result = listHelper.favoriteBlog(listWithOneBlog)
+        expect(result).toEqual(
+            {
+                _id: '5a422aa71b54a676234d17f8',
+                title: 'Go To Statement Considered Harmful',
+                author: 'Edsger W. Dijkstra',
+                url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+                likes: 5,
+                __v: 0
+            }
+        )
+    })
+
+    test('of a bigger list gives a right blog', () => {
+        const result = listHelper.favoriteBlog(listWithThreeBlogs)
+        expect(result).toEqual(
+            {
+                _id: '5a422aa71b54a676234d12f8',
+                title: "Testi blogi",
+                author: "Josuaki",
+                url: "josuaki.blogi.fi",
+                likes: 43,
+                __v: 0
+            }
+        )
+    })
 
 })
--- osa4/blogi/utils/list_helper.js
+++ osa4/blogi/utils/list_helper.js
@@ -4,7 +4,19 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => blogs.reduce((sum, blog) => sum + blog.likes, 0)
 
+const favoriteBlog = (blogs) => {
+    const reducer = (mostLikedBlog, comparingBlog) => {
+        if (mostLikedBlog.likes >= comparingBlog.likes ) {
+            return mostLikedBlog
+        } else {
+            return comparingBlog
+        }
+    }
+    return blogs.reduce(reducer, {})
+}
+
   module.exports = {
     dummy,
     totalLikes,
+    favoriteBlog,
   }
