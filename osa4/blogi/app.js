2131f3a7ce6ebcd58a15e5e45cc1a42cbddef3c9
Josua Kiviranta <josua.kiviranta@aalto.fi>
2019-09-09 17:44:53 +0300

    4.18: blogilistan laajennus, step7

--- osa4/blogi/app.js
+++ osa4/blogi/app.js
@@ -5,9 +5,11 @@ const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
+
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -25,6 +27,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 
 app.use(middleware.unknownEndpoint)
