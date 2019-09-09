187529565d6721800446ba6ad81d2cedd0276419
Josua Kiviranta <josua.kiviranta@aalto.fi>
2019-09-09 16:39:15 +0300

    4.17: blogilistan laajennus, step6

--- osa4/blogi/models/user.js
+++ osa4/blogi/models/user.js
@@ -12,7 +12,13 @@ const userSchema = mongoose.Schema({
         type: String,
         minlength: 3
     },
-    passwordHash: String
+    passwordHash: String,
+    blogs: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Blog'
+        }
+    ]
 })
 
 userSchema.plugin(uniqueValidator)
