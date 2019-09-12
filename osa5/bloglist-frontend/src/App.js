0b7cf8cf5bc4c82ffaa152d9e589812fbf851d0c
Josua Kiviranta <josua.kiviranta@aalto.fi>
2019-09-12 16:33:35 +0300

    5.1: blogilistan frontend, step1. Everything ok by now

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -1,12 +1,44 @@
-import React from 'react'
+import React , { useState, useEffect } from 'react'
 //import logo from './logo.svg';
 import './App.css';
+import blogService from './services/blogs'
+import Blog from './components/Blog'
+//import { thisExpression } from '@babel/types';
 
 function App() {
+const [blogs, setBlogs] = useState([])
+
+  useEffect(() =>{
+    blogService
+    .getAll()
+    .then(initialBlogs => setBlogs(initialBlogs))
+  }, [])
+
+  const rows = () => blogs.map(blog => 
+    <Blog
+      key={blog.id}
+      blog={blog}
+    />
+    )
+
   return (
     <div className="App">
+      <h1>Blogs</h1>
+
+      {/*<Notification message={errorMessage} />*/}
+
+      <h2>Login</h2>
+      {rows()}
+    </div> 
+    ); 
+}
+
+export default App;
+
+/*
+    <div className="App">
       <header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+      <img src={logo} className="App-logo" alt="logo" />*
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -20,7 +52,5 @@ function App() {
         </a>
       </header>
     </div>
-  );
-}
-
-export default App;
+    */
+    
