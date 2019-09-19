a39507fda2a0f6d1779bd54a9e23a45b57b46afc
Josua Kiviranta <josua.kiviranta@aalto.fi>
2019-09-19 20:00:33 +0300

    5.6* blogilistan frontend, step6

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import LikeButton from './LikeButton'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title}
-  </div>
-)
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div onClick={() => toggleVisibility()}>
+        <div style={hideWhenVisible}>
+          {blog.title} {blog.author}
+        </div>
+        <div style={showWhenVisible}>
+          <div>{blog.title}</div>
+          <a href={blog.url}>{blog.url}</a>
+          <div>
+            {blog.likes} likes <LikeButton/>
+          </div>
+          <div>added by {blog.author}</div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
