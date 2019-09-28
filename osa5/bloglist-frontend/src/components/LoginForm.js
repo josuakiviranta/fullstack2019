7cc7f0912020500b7f12def3eb153a53cdc0c403
Josua Kiviranta <josua.kiviranta@aalto.fi>
2019-09-28 12:39:01 +0300

    5.11: blogilistan frontend, step11

--- osa5/bloglist-frontend/src/components/LoginForm.js
+++ osa5/bloglist-frontend/src/components/LoginForm.js
@@ -1,31 +1,42 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 
-const LoginForm = ({ username, password, handleLogin, userCallback, passwordCallback }) => (
+const LoginForm = ({ username, password, handleLogin, userCallback, passwordCallback }) => {
+  return (
     <div>
-        <h1>log into application</h1>
-    <form onSubmit={handleLogin}>
-    <div>
-      username
+      <h1>log into application</h1>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
         <input
-        type="text"
-        value={username}
-        name="Username"
-        onChange={({ target }) => userCallback(target.value)}
-      />
-    </div>
-    <div>
-      password
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => userCallback(target.value)}
+          />
+        </div>
+        <div>
+          password
         <input
-        type="password"
-        value={password}
-        name="Password"
-        onChange={({ target }) => passwordCallback(target.value)}
-      />
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => passwordCallback(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
     </div>
-    <button type="submit">login</button>
-  </form>
-  </div>
-)
+  )
+}
+
+LoginForm.propTypes = {
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
+  handleLogin: PropTypes.func.isRequired,
+  userCallback: PropTypes.func.isRequired,
+  passwordCallback: PropTypes.func.isRequired
+}
 
 export default LoginForm
